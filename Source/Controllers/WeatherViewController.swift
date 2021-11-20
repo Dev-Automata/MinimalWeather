69e58fd6cbcd2d5100e8a328e308ca1fe654e41a
Kirill Mikhaylin <dev.automata@gmail.com>
2021-11-20 22:22:09 +0300

    Update outlets

--- Source/Controllers/WeatherViewController.swift
+++ Source/Controllers/WeatherViewController.swift
@@ -9,66 +9,67 @@ import UIKit
 import CoreLocation
 
 class WeatherViewController: UIViewController {
-
+    
     let weatherService = WeatherService()
     let locationService = LocationService()
-
+    
     var city: String?
-
+    
     @IBOutlet weak var cityNameLabel: UILabel!
-    @IBOutlet weak var weatherIcon: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
     @IBOutlet weak var pressureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var windLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         weatherService.delegate = self
         locationService.delegate = self
-
+        
         locationService.requestLocationOnAppStart()
-
+        
         if AppData.city != "" {
             weatherService.getForecastForCity(name: AppData.city)
         }
         
-        cityNameLabel.text = cityNameLabel.text?.uppercased()
+        weatherDescriptionLabel.text = weatherDescriptionLabel.text?.uppercased()
     }
-
+    
     
     @IBAction func locationPressed(_ sender: UIButton) {
         locationService.requestLocationOnUserDemand()
     }
-
+    
     @IBAction func unwindToWeatherScreen(_ unwindSegue: UIStoryboardSegue) {
         guard let city = city else { return }
         weatherService.getForecastForCity(name: city)
     }
-
+    
     private func updateUIOnWeatherLoad(with data: WeatherModel) {
-//        cityNameLabel.text = data.cityName
-//        weatherIcon.image = UIImage(systemName: data.conditionName)
-//        temperatureLabel.text = data.temperatureString
-//        pressureLabel.text = data.pressureString
-//        humidityLabel.text = data.humidityString
-//        windLabel.text = data.windSpeedString
+        cityNameLabel.text = data.cityName
+        weatherIcon.image = UIImage(systemName: data.conditionName)
+        temperatureLabel.text = data.temperatureString
+        pressureLabel.text = data.pressureString
+        humidityLabel.text = data.humidityString
+        windLabel.text = data.windSpeedString
     }
-
+    
 }
 
 // MARK: - WeatherServiceDelegate
 extension WeatherViewController: WeatherServiceDelegate {
-
+    
     func didUpdateWeather(_ weatherService: WeatherService, weather: WeatherModel) {
         AppData.city = weather.cityName
-
+        
         DispatchQueue.main.async {
             self.updateUIOnWeatherLoad(with: weather)
         }
     }
-
+    
     func didWeatherFailWithError(error: Error) {
         print(error)
     }
@@ -78,7 +79,7 @@ extension WeatherViewController: WeatherServiceDelegate {
 extension WeatherViewController: LocationServiceDelegate {
     func didAuthorizeRejected() {
         print("Location manager not authorized.")
-
+        
         if AppData.city != "" {
             city = AppData.city
             weatherService.getForecastForCity(name: city!)
@@ -86,14 +87,14 @@ extension WeatherViewController: LocationServiceDelegate {
             performSegue(withIdentifier: K.Segues.toCitySelectScreen, sender: self)
         }
     }
-
+    
     func didUpdateLocation(_ locationService: LocationService, latitude: Double, longitude: Double) {
         weatherService.getForecastForLocation(latitude: String(latitude), longitude: String(longitude))
     }
-
+    
     func didLocationFailWithError(error: Error) {
         print("Location manager error: \(error)")
         performSegue(withIdentifier: K.Segues.toCitySelectScreen, sender: self)
     }
-
+    
 }
